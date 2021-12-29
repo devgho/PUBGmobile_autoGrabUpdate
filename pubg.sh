05f59c14c02bae7a4bb51a58bb94e7b5e3a48ae0
devgho <devgho@outlook.com>
2021-12-29 14:55:33 +0800

    fix2

--- pubg.sh
+++ pubg.sh
@@ -1,9 +1,10 @@
 filename=`python3 /home/data/jymdata/auto_spider_apk/pubg/dl.py`
-name=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP name=\'.*?\'|grep -o \'.*\'`
-version_code=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP versionCode=\'.*?\'|grep -o \'.*\'`
-version_name=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP VersionName=\'.*?\'|grep -o \'.*\'`
+cd /home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/
+name=`/home/fujianfeng/android-9/aapt dump badging $filename|grep package|grep -oP name=\'.*?\'|grep -o \'.*\'`
+version_code=`/home/fujianfeng/android-9/aapt dump badging $filename|grep package|grep -oP versionCode=\'.*?\'|grep -o \'.*\'`
+version_name=`/home/fujianfeng/android-9/aapt dump badging $filename|grep package|grep -oP VersionName=\'.*?\'|grep -o \'.*\'`
 name=`echo $name|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
 version_code=`echo $version_code|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
 version_name=`echo $version_name|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
 echo "{\"version_code\":\"${version_code}\",\"version_name\":\"${version_name}\",\"last_filename\":\"${name}_${version_code}.apk\"}" > /home/data/jymdata/apks/$name.json
-cp /home/data/jymdata/auto_spider_apk/pubg/$filename /home/data/jymdata/apks/$name\_$version_code.apk
+cp $filename /home/data/jymdata/apks/$name\_$version_code.apk
