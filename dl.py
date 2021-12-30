de4100b1b31a59e7aac5843a2580380df54e7166
devgho <devgho@outlook.com>
2021-12-30 14:56:08 +0800

    absolute path

--- dl.py
+++ dl.py
@@ -11,8 +11,8 @@ all = res.text
 # print(res.text)
 url = re.search("https://dlied4.myapp.com/myapp/.*?apk",all).group()
 filename = re.search(r"(?P<u>.*/)(?P<filename>.*?apk)",url).group("filename")
-print(filename)
-if os.path.exists(filename):
+# print(filename)
+if os.path.exists("/home/data/jymdata/auto_spider_apk/pubg/"+filename):
     pass
 else:
     with open("/home/data/jymdata/auto_spider_apk/pubg/"+filename,"wb") as f:
